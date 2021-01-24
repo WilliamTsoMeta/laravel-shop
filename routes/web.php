e725fde8b3945ff08b829380103a1e1560b488bc
曹宇 <vitalitymail@foxmail.com>
2021-01-24 20:36:54 +0800

    邮箱验证中间件

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,14 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
+Route::group(['middleware'=>'auth'],function ()
+{
+  Route::get('/email_verify_notice','PagesController@emailVerifyNotice')->name('email_verify_notice');
+  Route::group(['middleware'=>'email_verified'],function(){
+    Route::get('/test',function(){
+      return 'you email...verified';
+    });
+  }); 
+});
+
 Auth::routes();
